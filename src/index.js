5791a9ccec51fac9bc0087eb0ce7aa47c3fb7aaf
dewanshpal108-crypto <dewanshpal108@gmail.com>
2026-08-04 21:07:25 +0530

    basic_setup

---
+++ src/index.js
@@ -0,0 +1,14 @@
+//express app
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello from the server!");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+}); 
